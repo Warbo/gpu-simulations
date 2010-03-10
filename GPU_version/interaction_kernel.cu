4a0b08fc8815e48da6ef8766bd1d24271da116c9
Chris Warburton <pha06cw@astrolab1.shef.ac.uk>
2010-03-10 12:02:52 +0000

    Parallelised interaction example

--- GPU_version/interaction_kernel.cu
+++ GPU_version/interaction_kernel.cu
@@ -8,12 +8,13 @@
 #include "linkedlists.c"
 
 // Each block needs access to its local particles
-__shared__ particle local_particles[2*10];
+__shared__ particle local_particles[2*32];
 
 // Define our kernel function
 __device__ void particles_interact(particle* particle_A, particle* particle_B) {
 	// Make two particles interact
 
+	/*
 	// Here we use a dummy gravity interaction
 	float x_delta, y_delta, z_delta, distance, x_unit, y_unit, z_unit;
 	x_delta = particle_B->x - particle_A->x;
@@ -30,81 +31,87 @@ __device__ void particles_interact(particle* particle_A, particle* particle_B) {
 	particle_A->x_acc += ((particle_A->mass * particle_B->mass)/(distance*distance))*x_unit;
 	particle_A->y_acc += ((particle_A->mass * particle_B->mass)/(distance*distance))*y_unit;
 	particle_A->z_acc += ((particle_A->mass * particle_B->mass)/(distance*distance))*z_unit;
+	*/
 
+	// Try doing a predictable interaction
+	particle_A->x_acc += 1;
+	
 }
 
-__device__ void calculate_from_buffer(particle* particle_A, particle* buffer, int buffer_size) {
-	// Calculate interactions between a particle and everything in our buffer
+__device__ void calculate_all_neighbours(int cell_size) {
+	// Calculate interactions between a particle and everything in the neighbour
 
-	// Loop through the buffer
-	for (unsigned int counter = 0; counter < buffer_size; counter++) {
-		// Update the particle based on the buffer contents
-		particles_interact(particle_A, &buffer[counter]);
+	// Loop through the size of the neighbour
+	for (unsigned int counter = 0; counter < cell_size; counter++) {
+		// Make our particle interact with everything in the second half of
+		// local memory
+		particles_interact(
+			&(local_particles[threadIdx.x]),
+			&(local_particles[cell_size + counter])
+						  );
 	}
 }
 
-__device__ void do_neighbouring_cells(int x_cell, int y_cell, int z_cell,
-int x_size, int y_size, int z_size, int cell_size) {
+__device__ void load_neighbouring_cells(int cell_size) {
 	// Loop through neighbours
-        for (int x_rel = -1; x_rel < 2; x_rel++) {
-                for (int y_rel = -1; y_rel < 2; y_rel++) {
-                        for (int z_rel = -1; z_rel < 2; z_rel++) {
+	for (int x_rel = -1; x_rel < 2; x_rel++) {
+		for (int y_rel = -1; y_rel < 2; y_rel++) {
+			for (int z_rel = -1; z_rel < 2; z_rel++) {
 
-                                // Only act if we've got a valid neighbour
-                                if (
-                                        (x_cell+x_rel >= 0) && (x_cell+x_rel < x_size) &&
-                                        (y_cell+y_rel >= 0) && (y_cell+y_rel < y_size) &&
-                                        (z_cell+z_rel >= 0) && (z_cell+z_rel < z_size)
-                                ) {
+				// Only act if we've got a valid neighbour
+				if (
+					(blockIdx.xl+x_rel >= 0) &&
+					(blockIdx.x+x_rel < gridDim.x) &&
+					(blockIdx.y+y_rel >= 0) &&
+					(blockIdx.y+y_rel < gridDim.y) &&
+					(blockIdx.z+z_rel >= 0) &&
+					(blockIdx.z+z_rel < gridDim.z)
+				) {
 
-					// Load neighbour to shared memory
-					int offset = (z_size*y_size*(x_cell+x_rel)) + (z_size*(y_cell+y_rel)) + (z_cell+z_rel);
-				
-					for (int count = 0; count < cell_size; count++) {
-						local_particles[cell_size+count] = all_particles[(cell_size*offset)+count];
-					}
-	
-					// TODO: DON'T LOOP HERE, SET UP SOME THREADS AND MAKE THEM SYNCHRONISE			
-					for (int count = 0; count < cell_size; count++) {
-						calculate_from_buffer(&(local_particles[count]), 
-&(local_particles[cell_size]), cell_size);
-					}
+					// Load neighbouring cell to shared memory
 
+					// This is the memory location we need to start allocating
+					// from
+					int offset = (gridDim.z * gridDim.y * (blockIdx.x + x_rel))
+					+ (gridDim.z * (blockIdx.y + y_rel)) + (blockIdx.z + z_rel);
+
+					// Each thread allocates a particle from the neighbour
+					// Start at cell_size since we're filling the second half
+					local_particles[cell_size + threadIdx.x] =
+						all_particles[(cell_size * offset) + threadIdx.x];
+
+					// Ensure all particles have been loaded into shared mem
+					__syncthreads();
+
+					// Work out the interactions with these neighbours
+					calculate_all_neighbours(&(local_particles[count]),
+						&(local_particles[cell_size]), cell_size);
+
+					// Ensure everyone's finished before loading the next cell
+					__syncthreads();
+					
 				}
 			}
 		}
 	}
 }
 
-//extern __shared__ particle current_particles
-__device__ void do_particle(particle* particle_A, int x, int y, int z) {
-	// Start off with no acceleration
-	particle_A->x_acc = 0.0;
-	particle_A->y_acc = 0.0;
-	particle_A->z_acc = 0.0;
-
-	// Calculate all interactions	
-	do_neighbouring_cells(particle_A, x, y, z);
-}
-
-__device__ __global__ void do_cell(particle* all_particles, int x_size, int y_size, int z_size, int x, int y, int z, int 
-cell_size) {
-	// This sorts out memory for each cell thread
+__global__ void do_cell(particle* all_particles, int cell_size) {
+	// This begins calculation of the particle interactions
 
 	// Work out where in the array our particles start
-	int offset = (z_size*y_size*x) + (z_size*y) + z;
+	int offset = (gridDim.z*gridDim.y*blockIdx.x) + (gridDim.z*blockIdx.y)
+			+ blockIdx.z;
 
-	// Allocate some local storage for them
-	// TODO: At the moment, treat all cells as having cell_size particles
-	// Allocating 2*cell_size gives us our local cell and a neighbour cell
-	//__shared__ particle local_particles[2*cell_size];
+	// Each thread loads its own particle to local memory
+	local_particles[threadIdx.x] =
+		all_particles[(cell_size * offset) + threadIdx.x];
 
-	for (int count = 0; count < cell_size; count++) {
-		local_particles[count] = all_particles[(cell_size*offset)+count];
-	}
+	// Initialise the interaction values
+	local_particles[threadIdx.x].x_acc = 0.0;
 	
 	// Now load in our neighbours and calculate interactions
-	do_neighbouring_cells(x, y, z, x_size, y_size, z_size, cell_size);
+	load_neighbouring_cells(cell_size);
 }
 
 int main() {
