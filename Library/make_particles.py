e61d0939bc3d8b0d2a58ca5164538b5e2d281054
Chris Warburton <chris@chris-desktop.(none)>
2010-05-20 00:54:30 +0100

    IDs are now ints as expected

--- Library/make_particles.py
+++ Library/make_particles.py
@@ -6,46 +6,53 @@ import random
 
 # For stripping away exponential notation.
 # Taken from http://code.activestate.com/recipes/358361/
-def non_exp_repr(x):
-    """Return a floating point representation without exponential notation.
+def non_exp_repr(x, is_int=False):
+	"""Return a floating point representation without exponential notation.
 
-    Result is a string that satisfies:
-        float(result)==float(x) and 'e' not in result.
-    
-    >>> non_exp_repr(1.234e-025)
-    '0.00000000000000000000000012339999999999999'
-    >>> non_exp_repr(-1.234e+018)
-    '-1234000000000000000.0'
-    
-    >>> for e in xrange(-50,51):
-    ...     for m in (1.234, 0.018, -0.89, -75.59, 100/7.0, -909):
-    ...         x = m * 10 ** e
-    ...         s = non_exp_repr(x)
-    ...         assert 'e' not in s
-    ...         assert float(x) == float(s)
+	Result is a string that satisfies:
+		float(result)==float(x) and 'e' not in result.
+	
+	>>> non_exp_repr(1.234e-025)
+	'0.00000000000000000000000012339999999999999'
+	>>> non_exp_repr(-1.234e+018)
+	'-1234000000000000000.0'
+	
+	>>> for e in xrange(-50,51):
+	...	 for m in (1.234, 0.018, -0.89, -75.59, 100/7.0, -909):
+	...		 x = m * 10 ** e
+	...		 s = non_exp_repr(x)
+	...		 assert 'e' not in s
+	...		 assert float(x) == float(s)
 
-    """
-    s = repr(float(x))
-    e_loc = s.lower().find('e')
-    if e_loc == -1:
-        return s
+	"""
+	if is_int:
+		s = repr(int(x))
+	else:
+		s = repr(float(x))
+	e_loc = s.lower().find('e')
+	if e_loc == -1:
+		return s
 
-    mantissa = s[:e_loc].replace('.', '')
-    exp = int(s[e_loc+1:])
+	mantissa = s[:e_loc].replace('.', '')
+	exp = int(s[e_loc+1:])
 
-    assert s[1] == '.' or s[0] == '-' and s[2] == '.', "Unsupported format"     
-    sign = ''
-    if mantissa[0] == '-':
-        sign = '-'
-        mantissa = mantissa[1:]
+	assert s[1] == '.' or s[0] == '-' and s[2] == '.', "Unsupported format"	 
+	sign = ''
+	if mantissa[0] == '-':
+		sign = '-'
+		mantissa = mantissa[1:]
 
-    digitsafter = len(mantissa) - 1     # num digits after the decimal point
-    if exp >= digitsafter:
-        return sign + mantissa + '0' * (exp - digitsafter) + '.0'
-    elif exp <= -1:
-        return sign + '0.' + '0' * (-exp - 1) + mantissa
-    ip = exp + 1                        # insertion point
-    return sign + mantissa[:ip] + '.' + mantissa[ip:]
+	digitsafter = len(mantissa) - 1	 # num digits after the decimal point
+	if exp >= digitsafter:
+		if is_int:
+			return sign + mantissa + '0'*(exp - digitsafter)
+		return sign + mantissa + '0' * (exp - digitsafter) + '.0'
+	elif exp <= -1:
+		return sign + '0.' + '0' * (-exp - 1) + mantissa
+	ip = exp + 1						# insertion point
+	if is_int:
+		return sign + mantissa[:ip]
+	return sign + mantissa[:ip] + '.' + mantissa[ip:]
 
 def rand(low, high):
 	return (random.random() * (high-low)) + low
@@ -70,7 +77,7 @@ zmax = float(zmax)
 
 for p in range(int(number)):
 	outfile.write(\
-		non_exp_repr(p)+','+\
+		non_exp_repr(p, True)+','+\
 		non_exp_repr(round(rand(xmin,xmax), 5))+','+\
 		non_exp_repr(round(rand(ymin,ymax), 5))+','+\
 		non_exp_repr(round(rand(zmin,zmax), 5))+\
